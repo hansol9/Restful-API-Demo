6c7242455e66fb763744ca517e5aa61d93ee5cdb
hansol9 <hansol9@gmail.com>
2022-08-23 10:26:10 -0400

    test

---
+++ src/main/java/hansol9/restfulapi/RestfulApiDemoApplication.java
@@ -0,0 +1,14 @@
+package hansol9.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestfulApiDemoApplication.class, args);
+    }
+
+}
